f2f8615d5ce78676ec7e5345bb63e67ffb3c0eb6
Changsu <mgs1527@icloud.com>
2023-03-22 22:17:34 +0900

    [feat] omo 패키지 커밋

---
+++ src/main/java/com/ppiyong/e312/omo/repository/MafiaChartRepository.java
@@ -0,0 +1,8 @@
+package com.ppiyong.e312.omo.repository;
+
+import com.ppiyong.e312.omo.entity.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MafiaChartRepository extends JpaRepository<Person, Long> {
+
+}
