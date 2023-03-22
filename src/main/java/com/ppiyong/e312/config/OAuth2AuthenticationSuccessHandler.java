ef5200412e6a523defdce60a3e4283b5348520c9
Changsu <mgs1527@icloud.com>
2023-03-22 12:54:22 +0900

    [feat] react 와 로그인 기능 맞추기

--- src/main/java/com/ppiyong/e312/config/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/ppiyong/e312/config/OAuth2AuthenticationSuccessHandler.java
@@ -8,8 +8,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -23,7 +25,25 @@ public class OAuth2AuthenticationSuccessHandler implements AuthenticationSuccess
         User user=pr.getUser();
         String jwtToken=jwtProvider.createToken(user);
         response.setHeader(JwtProperties.HEADER_STRING, JwtProperties.TOKEN_PREFIX + jwtToken);
-
-      response.sendRedirect("http://localhost:3000");
+        Cookie cookie =new Cookie(JwtProperties.HEADER_STRING,jwtToken);
+        response.addCookie(cookie);
+        String uri=makeRedirectUrl(jwtToken);
+        System.out.println("uri = " + uri);
+//      response.sendRedirect("http://192.168.31.158:3000/login");
+        response.sendRedirect(uri);
+    }
+    public String onAuthenticationSuccess2(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        PrincipalDetails pr= (PrincipalDetails) authentication.getPrincipal();
+        User user=pr.getUser();
+        String jwtToken=jwtProvider.createToken(user);
+        response.setHeader(JwtProperties.HEADER_STRING, JwtProperties.TOKEN_PREFIX + jwtToken);
+        Cookie cookie =new Cookie(JwtProperties.HEADER_STRING,jwtToken);
+        response.addCookie(cookie);
+        System.out.println("성공2함수실행");
+        return jwtToken;
+    }
+    private String makeRedirectUrl(String token) {
+        return UriComponentsBuilder.fromUriString("http://localhost:3000/"+token)
+                .build().toUriString();
     }
 }
